9d24d76bc1b338f90f173c4f41a53c01067a5725
florian0@project-hazard.org <florian0@project-hazard.org@9d37acd1-93be-12f9-c804-3522b33c7b0f>
2011-03-28 21:36:30 +0000

    Updated the Coordinate-Helper-Class

--- Helpers/Coordinate.cs
+++ Helpers/Coordinate.cs
@@ -2,17 +2,22 @@
 using System.Text;
 using System.Drawing;
 
-namespace BotManager.Helpers
+namespace BotManager
 {
+    /// <summary>
+    /// Coordinate Conversion Helper Class
+    /// </summary>
     class Coordinate
     {
         private Point InGameCoord;
 
+        // IMPORTANT: InGame (0|0) = Pixel (0|0) at Sector (135|91)
+
         #region Constructor
         /// <summary>
-        /// Erstellt ein neues Coordinate-Objekt
+        /// Creates a new Coordinate Object
         /// </summary>
-        /// <param name="Coord">Silkroad InGame Koordinate</param>
+        /// <param name="Coord">Silkroad InGame Coordinate</param>
         public Coordinate(Point Coord)
         {
             InGameCoord = Coord;
@@ -21,7 +26,7 @@ namespace BotManager.Helpers
 
         #region Sector
         /// <summary>
-        /// Liest den Sektor der Koordinate ab (readonly)
+        /// Returns the Sector of the Coordinate (readonly)
         /// </summary>
         public Point Sector
         {
@@ -30,22 +35,22 @@ namespace BotManager.Helpers
                 int xSec = 0;
                 int ySec = 0;
 
-                if (centercoord.X >= 0)
+                if (InGameCoord.X >= 0)
                 {
-                    xSec = (centercoord.X / 192) + 135;
+                    xSec = (InGameCoord.X / 192) + 135;
                 }
-                else if (centercoord.X < 0)
+                else if (InGameCoord.X < 0)
                 {
-                    xSec = 134 - (centercoord.X / 192);
+                    xSec = 134 - (InGameCoord.X / 192);
                 }
 
-                if (centercoord.Y >= 0)
+                if (InGameCoord.Y >= 0)
                 {
-                    ySec = (centercoord.Y / 192) + 92;
+                    ySec = (InGameCoord.Y / 192) + 92;
                 }
-                else if (centercoord.Y < 0)
+                else if (InGameCoord.Y < 0)
                 {
-                    ySec = 91 - (centercoord.Y / 192);
+                    ySec = 91 - (InGameCoord.Y / 192);
                 }
 
                 return new Point(xSec, ySec);
@@ -54,14 +59,62 @@ namespace BotManager.Helpers
         #endregion
 
         #region RealCoordinates
+        // Idea:
+        // Caluclate with InGame Coordinates first and Convert the remaining Value into Pixel
+        // Might be more accurate
         public Point RealCoordinates
         {
             get
             {
+                int sectordiff_x;
+                int pixeldiff_x;
 
+                if (Sector.X >= 135)
+                {
+                    // Sector >= 135 means our coordinate is right of (0|0)
+                    sectordiff_x = Sector.X - 135;
+                    pixeldiff_x = AsPixel().X - (sectordiff_x * 256);
+                }
+                else
+                {
+                    sectordiff_x = 135 - Sector.X;
+                    pixeldiff_x = (sectordiff_x * 256) + AsPixel().X;
+                }
+
+
+                int sectordiff_y;
+                int pixeldiff_y;
+
+                if (Sector.Y <= 91)
+                {
+                    // Sector <= 91 means our coordinate is lower than (0|0)
+                    sectordiff_y = 91 - Sector.Y;
+                    pixeldiff_y = AsPixel().Y - (sectordiff_y * 256);
+                }
+                else
+                {
+                    sectordiff_y = Sector.Y - 91;
+                    pixeldiff_y = (sectordiff_y * 256) + AsPixel().Y;
+                }
+
+
+                return new Point(pixeldiff_x, pixeldiff_y);
             }
 
         }
         #endregion
+
+
+        /// <summary>
+        /// Convert InGame Coordinates into Pixels
+        /// </summary>
+        /// <returns>InGame Coordinate as Pixel</returns>
+        private Point AsPixel()
+        {
+            return new Point(
+                Convert.ToInt32((double)InGameCoord.X * 1.333333333),
+                Convert.ToInt32((double)InGameCoord.Y * 1.333333333)
+                );
+        }
     }
 }
